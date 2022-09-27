6e4536c1779e2e0ac870af0aed84c9f7860d7c78
estravagancia <webmaster@estravagancia.com>
2022-09-27 09:39:01 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const recognition = new webkitSpeechRecognition()
+recognition.lang = 'es-ES'
+recognition.continuous = true
+recognition.onresult = event => {
+	for (const result of event.results) {
+		console.log(result[0].transcript)
+	}
+}
+document.body.onclick = function () {
+  recognition.start();
+  console.log("Lista para escuchar.");
+};
+recognition.onspeechend = function () {
+	recognition.stop();
+  console.log("Parado. Pulsa en la página para volver a escuchar");
+};
